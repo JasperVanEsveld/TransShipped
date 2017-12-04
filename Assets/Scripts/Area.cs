838e957904a7849cec661b455804e5baa0efbfa7
CoolTomatos <CoolTomatos@iCloud.com>
2017-12-04 22:45:05 +0100

    Basic frames of the c# scripts

---
+++ Assets/Scripts/Area.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Area : MonoBehaviour
+{
+    ContainerManager _manager;
+    List<ContainerProcessor> _connected;
+    protected abstract bool AddContainer(MonoContainer monoContainer);
+}
---
+++ Assets/Scripts/ContainerManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+public class ContainerManager
+{
+    List<DeliveryArea<DeliveryVehicle>> _deliver;
+    List<Stack> _stacks;
+
+    void OnMovementComplete(Movement movement)
+    {
+    }
+
+    void Store(MonoContainer container)
+    {
+    }
+
+    void Request(Area area, Container container)
+    {
+    }
+
+    Area GetNextArea(Area area, Movement movement)
+    {
+        return null;
+    }
+}
