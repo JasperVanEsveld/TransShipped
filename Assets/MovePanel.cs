e69d4006ca5979c8f68fe30131d478028de9defc
Felix Yang <felixyang117+GitHub@hotmail.com>
2017-12-05 13:11:50 +0100

    ui script

---
+++ Assets/MovePanel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MovePanel : MonoBehaviour {
+    private int state = 0;
+    public void HidePanel()
+    {
+        if (state == 0)
+        {
+            RectTransform rt = transform.parent.GetComponent<RectTransform>();
+            rt.anchorMin = new Vector2(0.05f, -0.3f);
+            rt.anchorMax = new Vector2(0.95f, 0.0f);
+            state = 1;
+        }
+        else
+        {
+            RectTransform rt = transform.parent.GetComponent<RectTransform>();
+            rt.anchorMin = new Vector2(0.05f, 0.0f);
+            rt.anchorMax = new Vector2(0.95f, 0.3f);
+            state = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/ui_mouseover.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ui_mouseover : MonoBehaviour {
+
+    // Use this for initialization
+    void OnMouseOver()  
+    {
+        //If your mouse hovers over the GameObject with the script attached, output this message
+        //Debug.Log("Mouse is over GameObject.");
+        RectTransform rt = GameObject.Find("Canvas/MouseoverTooltipPanel").GetComponent<RectTransform>();
+        
+
+        rt.anchorMin = new Vector2(0.00f, 0.4f);
+        rt.anchorMax = new Vector2(0.1f, 0.8f);
+        Text txt = GameObject.Find("Canvas/MouseoverTooltipPanel/Text").GetComponent<Text>();
+        txt.text = transform.name;
+    }
+
+    void OnMouseExit()
+    {
+        //The mouse is no longer hovering over the GameObject so output this message each frame
+        // Debug.Log("Mouse is no longer on GameObject.");
+        RectTransform rt = GameObject.Find("Canvas/MouseoverTooltipPanel").GetComponent<RectTransform>();
+        rt.anchorMin = new Vector2(0.42f, -0.7f);
+        rt.anchorMax = new Vector2(0.58f, -0.3f);
+    }
+}
