f8eab84b5b5462db47d1ecbcd6142739835e648a
Jasper van Esveld <jaspervanesveld@gmail.com>
2017-12-08 18:27:22 +0100

    Move some UI scripts to another folder

---
+++ Assets/Scripts/UI/StatePanelManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatePanelManager : MonoBehaviour {
+
+	private Game game;
+	// Use this for initialization
+	void Start () {
+		game = FindObjectOfType<Game>();
+		game.stateChangeEvent += new OnStateChanged(stateChanged);
+	}
+
+	public void stateChanged(GameState newState) {
+
+	}
+}
