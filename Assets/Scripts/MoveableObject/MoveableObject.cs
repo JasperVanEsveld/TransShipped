eeba2cd6ff451a8ee4cb115e96c5289fd40cd84f
Felix Yang <felixyang117+GitHub@hotmail.com>
2017-12-18 21:18:52 +0100

    New Movement Class

---
+++ Assets/Scripts/MoveableObject/MoveableObject.cs
@@ -0,0 +1,277 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MoveableObject : MonoBehaviour
+{
+    // HOW TO USE:
+    // for all vehicles (child class of this class), call Init() in their Start();
+    // for all vehicles (child class of this class), call MovementUpdate() in their Update(); It will take care of all the movement thinggy
+    // For ship, invoke EnterTerminal(Vector3 i_dest) and LeaveTerminal() to... have it enter and leave. 
+    // The i_dest should be the position of a parking spot for the ship, the east one of the west one.
+    // You can add a sth in the end of LeaveTerminal() to have the script destroy the GameObject after leaving the screen.
+    // For ground vehicle, just invoke PushNewDest(Vector3 i_dest) to have it move to that position. (the position has to on the ground, obviously)
+    // You can utilize IsObjectMoving() and IsAtTheThisPos(Vector3 i_pos) to help you with debugging, but ideally you should not need to use them anymore.
+
+
+    // CALL THIS IN Update() IN WHICHEVER CHILD CLASS YOU ARE USING
+    // It will takes care of all the movement stuff so you would not need to worry about them in child class anymore.
+    protected void MovementUpdate()
+    {
+        transform.position = CalcPosForNextFrame_();
+    }
+
+    // CALL THIS IN Start() FIRST IN WHICHEVER CHILD CLASS YOU ARE USING
+    protected void Init(Vector3 i_initPos, float i_speed, bool i_isAtSeaLevel, Vector3 i_scale)
+    {
+        movementQueue_ = new Queue<Vector3>();
+        lastPos_ = transform.position;
+        transform.position = new Vector3(i_initPos.x, height_, i_initPos.z);
+        speed_ = i_speed;
+        isSeaVehicle_ = i_isAtSeaLevel;
+        if (isSeaVehicle_)
+            height_ = -2.0f;
+        else
+            height_ = 0.0f;
+        transform.localScale = i_scale;
+    }
+
+    // or this if you dont want to modify the scale
+    protected void Init(Vector3 i_initPos, float i_speed, bool i_isAtSeaLevel)
+    {
+        movementQueue_ = new Queue<Vector3>();
+        lastPos_ = transform.position;
+        transform.position = new Vector3(i_initPos.x, height_, i_initPos.z);
+        speed_ = i_speed;
+        isSeaVehicle_ = i_isAtSeaLevel;
+        if (isSeaVehicle_)
+            height_ = -2.0f;
+        else
+            height_ = 0.0f;
+    }
+
+    // If the object is not moving/ does not have anywhere to go, return true
+    public bool IsObjectMoving()
+    {
+        if (movementQueue_.Count != 0)
+            return true;
+        else
+            return false;
+    }
+
+    // For ground vehicles, tell to object to move to this place when it's done with wtever it is doing
+    public void PushNewDest(Vector3 i_dest)
+    {
+        int desRegion = GetRegion_(i_dest);
+        int curRegion = GetRegion_(lastPos_);
+        Debug.Log("desRegion: " + desRegion + " curRegion: " + curRegion);
+        if (curRegion == desRegion)
+        {
+            movementQueue_.Enqueue(new Vector3(lastPos_.x, height_, i_dest.z));
+            movementQueue_.Enqueue(i_dest);
+            lastPos_ = i_dest;
+        }
+        else
+        {
+            if (desRegion == 1)
+            {
+                movementQueue_.Enqueue(new Vector3(lastPos_.x, height_, i_dest.z));
+                movementQueue_.Enqueue(i_dest);
+                lastPos_ = i_dest;
+            }
+            else if ((desRegion == 0) && (curRegion == 2) || (desRegion == 2) && (curRegion == 0))
+            {
+                movementQueue_.Enqueue(new Vector3(lastPos_.x, height_, i_dest.z));
+                movementQueue_.Enqueue(i_dest);
+                lastPos_ = i_dest;
+            }
+            else if (desRegion == 4)
+            {
+                movementQueue_.Enqueue(new Vector3(lastPos_.x, height_, i_dest.z));
+                movementQueue_.Enqueue(i_dest);
+                lastPos_ = i_dest;
+            }
+            else
+            {
+                movementQueue_.Enqueue(new Vector3(i_dest.x, height_, lastPos_.z));
+                movementQueue_.Enqueue(i_dest);
+                lastPos_ = i_dest;
+            }
+        }
+    }
+
+    // Check if the object is at this place
+    public bool IsAtTheThisPos(Vector3 i_pos)
+    {
+        Vector3 cur = transform.position;
+        cur.y = height_;
+        Vector3 des = i_pos;
+        des.y = height_;
+
+        float dis = Vector3.Distance(cur, des);
+        if (dis <= speed_ * Time.deltaTime)
+            return true;
+        else
+            return false;
+    }
+
+    // For ships, call this to have it enter terminal.
+    // args: the position of the terminal area
+    public void EnterTerminal(Vector3 i_dest)
+    {
+        int desRegion = GetRegion_(i_dest);
+        Debug.Log(desRegion);
+        if (desRegion == 5)
+        {
+            Vector3 intPoint2 = new Vector3(i_dest.x, height_, 36.0f);
+            Vector3 intPoint1 = new Vector3(80.0f, height_, 36.0f);
+            movementQueue_.Enqueue(intPoint1);
+            movementQueue_.Enqueue(intPoint2);
+            movementQueue_.Enqueue(i_dest);
+        }
+        else if (desRegion == 3)
+        {
+            Vector3 intPoint2 = new Vector3(i_dest.x, height_, -36.0f);
+            Vector3 intPoint1 = new Vector3(80.0f, height_, -36.0f);
+            movementQueue_.Enqueue(intPoint1);
+            movementQueue_.Enqueue(intPoint2);
+            movementQueue_.Enqueue(i_dest);
+        }
+    }
+
+    // For ships, call this to have it leave terminal.
+    public void LeaveTerminal()
+    {
+        int curRegion = GetRegion_(transform.position);
+        if (curRegion == 5)
+        {
+            Vector3 intPoint1 = new Vector3(transform.position.x, height_, 36.0f);
+            Vector3 intPoint2 = new Vector3(80.0f, height_, 36.0f);
+            movementQueue_.Enqueue(intPoint1);
+            movementQueue_.Enqueue(intPoint2);
+        }
+        else if (curRegion == 3)
+        {
+            Vector3 intPoint1 = new Vector3(transform.position.x, height_, -36.0f);
+            Vector3 intPoint2 = new Vector3(80.0f, height_, -36.0f);
+            movementQueue_.Enqueue(intPoint1);
+            movementQueue_.Enqueue(intPoint2);
+        }
+        // TODO: Delete the parent GameObject
+    }
+
+
+
+
+
+    // ------------------- You should not need to worry about these below -----------------------//
+    private Queue<Vector3> movementQueue_;
+    private Vector3 lastPos_;
+    private float speed_;
+    private float height_;
+    private bool isSeaVehicle_;
+
+    // Check if the object has finished the current movement task
+    private bool IsAtTheCurrentDest_()
+    {
+        if (!IsObjectMoving()) return true;
+        else
+        {
+            Vector3 cur = transform.position;
+            cur.y = height_;
+            Vector3 des = movementQueue_.Peek();
+            des.y = height_;
+
+            float dis = Vector3.Distance(cur, des);
+            if (dis <= speed_ * Time.deltaTime)
+                return true;
+            else
+                return false;
+        }
+    }
+
+    // Return a pos that the object is supposed to be for the next frame
+    private Vector3 CalcPosForNextFrame_()
+    {
+        if ((!IsObjectMoving()))
+        {
+            return transform.position;
+        }
+        else
+        {
+            if (IsAtTheCurrentDest_())
+            {
+                movementQueue_.Dequeue();
+                if (!IsObjectMoving())
+                {
+                    return transform.position;
+                }
+                
+            }
+            float step = speed_ * Time.deltaTime;
+            Vector3 cur = transform.position;
+            cur.y = height_;
+            Vector3 des = movementQueue_.Peek();
+            des.y = height_;
+            return Vector3.MoveTowards(cur, des, step);
+        }
+    }
+
+
+    private int GetRegion_(Vector3 i_vec)
+    {
+        float zp = 12.0f;
+        float zn = -12.0f;
+        float xs = 30.0f;
+        float xc = 1.0f;
+        float x = i_vec.x;
+        float z = i_vec.z;
+
+        if (z <= zn)
+        {
+            if (x <= xc)
+            {
+                return 0;
+            }
+            else if (x >= xs)
+            {
+                return 6;
+            }
+            else
+            {
+                return 3;
+            }
+        }
+        else if (z >= zp)
+        {
+            if (x <= xc)
+            {
+                return 2;
+            }
+            else if (x >= xs)
+            {
+                return 8;
+            }
+            else
+            {
+                return 5;
+            }
+        }
+        else
+        {
+            if (x <= xc)
+            {
+                return 1;
+            }
+            else if (x >= xs)
+            {
+                return 7;
+            }
+            else
+            {
+                return 4;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/MoveableObject/MoveableObject_Ship.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveableObject_Ship : MoveableObject
+{
+    
+
+    // Use this for initialization
+    void Start () {
+        // You may want to write sth before this to determine the exact parameters to pass to Init();
+        Init(new Vector3(65.0f, 0.0f, 35.0f), 20.0f, true, new Vector3(20.0f, 2.0f, 2.0f));
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        MovementUpdate();
+
+    }
+}
