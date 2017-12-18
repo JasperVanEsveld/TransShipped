b79f218d94a8d2f670e4e5a6d33b56ca699cc11b
CoolTomatos <CoolTomatos@iCloud.com>
2017-12-18 11:59:16 +0100

    Click an OptionalArea and buy it.

--- Assets/Scripts/Area/OptionalArea.cs
+++ Assets/Scripts/Area/OptionalArea.cs
@@ -6,27 +6,37 @@ public class OptionalArea : MonoBehaviour
     private Game game { get; set; }
 
     public List<Area> connected;
+
     public string areaName;
-    public Transform prefab;
+
+    private GameObject stackPrefab;
 
     public double price = 10;
 
     private bool locked;
 
-    private int i = 0;
+    private int i;
 
-    private void Start() {
+    private Color originColor;
+
+    private readonly Color stackColor = new Color32(0xC0, 0xC0, 0xC0, 0xFF);
+
+    private void Start()
+    {
         game = (Game) FindObjectOfType(typeof(Game));
+        stackPrefab = Resources.Load("Areas/Stack") as GameObject;
+        GetComponent<Renderer>().material.color = price <= game.money ? Color.green : Color.red;
         game.RegisterArea(this);
     }
 
+    [System.Obsolete("This function is meant for the building panel, now it is obsolete.")]
     public void BuyArea()
     {
         if (!(game.currentState is UpgradeState)) return;
         if (((UpgradeState) game.currentState).Buy(price))
         {
             game.optionalAreas.Remove(this);
-            var stack = Instantiate(prefab, transform.position, transform.rotation).GetComponent<Stack>();
+            var stack = Instantiate(stackPrefab, transform.position, transform.rotation).GetComponent<Stack>();
             stack.max = 5 * (int) ((transform.lossyScale.x - 2) / 2) * (int) (transform.lossyScale.z - 2);
             foreach (var connectArea in connected)
             {
@@ -41,48 +51,49 @@ public class OptionalArea : MonoBehaviour
         i++;
     }
 
+    private void OnMouseEnter()
+    {
+        if (!(game.currentState is UpgradeState)) return;
+        GetComponent<Renderer>().material.color = stackColor;
+    }
 
-//---- Implement this so that we can get rid of the clunky buy menu?   
-    private Color startingColor_;
-    void OnMouseEnter()
+    private void OnMouseExit()
     {
-        // if (in BUILDING stage)
-        //{ 
-            startingColor_ = GetComponent<Renderer>().material.color; // Move this to Start() so that it only needs to be exec once.
-            GetComponent<Renderer>().material.color = new Color(1.0f, 0.0f, 0.0f, 1.0f);
-        //}
+        if (!(game.currentState is UpgradeState)) return;
+        GetComponent<Renderer>().material.color = originColor;
     }
-    void OnMouseExit()
+
+    private void OnMouseDown()
     {
-        // if (in BUILDING stage)
-        //{
-            GetComponent<Renderer>().material.color = startingColor_;
-        //}
+        if (!(game.currentState is UpgradeState)) return;
+        if (((UpgradeState) game.currentState).Buy(price))
+        {
+            game.optionalAreas.Remove(this);
+            var stack = Instantiate(stackPrefab, transform.position, transform.rotation).GetComponent<Stack>();
+            stack.max = 5 * (int) ((transform.lossyScale.x - 2) / 2) * (int) (transform.lossyScale.z - 2);
+            foreach (var connectArea in connected)
+            {
+                stack.Connect(connectArea);
+            }
+            Destroy(gameObject);
+        }
+        else if (i == 0)
+        {
+            print("You don't have enough moeny");
+        }
+        i++;
     }
-    void OnMouseDown()
+
+    private void Update()
     {
-        // if (in BUILDING stage && have enough money) 
-        //{
-            Debug.Log(name + "selected.");
-        
-       // }
-    }
-    // Called when entering operation stage. If not bought.
-    public void HideAreaNotBought()
-    {
-        //if (not bought)
-        //{
+        if (!(game.currentState is UpgradeState))
+        {
             GetComponent<MeshRenderer>().enabled = false;
-        //}
-    }
-    // Called when entering another buy stage, if previously hidden.
-    public void ShowAreaNotBought()
-    {
-        //if (not bought)
-        //{
-            //GetComponent<Renderer>().material.color = startingColor_;
+        }
+        else
+        {
             GetComponent<MeshRenderer>().enabled = true;
-        //}
+            originColor = price <= game.money ? Color.green : Color.red;
+        }
     }
-    //---- Implement the above section so that we can get rid of the clunky buy menu?   ---------/
 }
