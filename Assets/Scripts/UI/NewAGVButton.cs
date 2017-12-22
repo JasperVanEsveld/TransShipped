00303711aa1d72a61b78fe312bcf47b76268c4cd
Arjan van Schendel <arjanvanschendel@gmail.com>
2017-12-22 17:04:38 +0100

    Add method to add AGV

---
+++ Assets/Scripts/UI/NewAGVButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class NewAGVButton : MonoBehaviour {
+
+    private Game game;
+    private Road road;
+    
+    void Start () {
+        game = FindObjectOfType<Game>();
+        road = FindObjectOfType<Road>();
+        OnMouseDown();
+    }
+
+    private void OnMouseDown()
+    {
+        GameObject v = Instantiate(GameObject.Find("Vehicle"));
+        road.vehicles.Add(v.GetComponent(typeof(Vehicle)) as Vehicle);
+    }
+}
