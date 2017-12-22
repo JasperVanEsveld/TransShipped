bf3154903ea35a55cd864b7099d558d303aad9f2
Jasper van Esveld <jaspervanesveld@gmail.com>
2017-12-22 16:38:27 +0100

    Fix SimpleScene

--- Assets/SunRotation.cs
+++ Assets/SunRotation.cs
@@ -5,29 +5,22 @@ using UnityEngine;
 public class SunRotation : MonoBehaviour {
 
 
-    public Transform lightOrigin;
     public float rotationSpeed;
     public float xAngle;
     public float yAngle;
     public float zAngle;
-    public Game game;
+    private Game game;
 
 
-    // Use this for initialization
     void Start () {
         game = FindObjectOfType<Game>();
         transform.rotation = Quaternion.Euler(xAngle, yAngle, zAngle);
     }
 	
-	// Update is called once per frame
-
 	void Update () {
         if ((game.currentState is OperationState))
         {
             transform.Rotate(Vector3.right * Time.deltaTime * rotationSpeed);
         }
-            
-
-        //transform.RotateAround(lightOrigin.position, Vector3.right, Time.deltaTime * rotationSpeed);
     }
 }
