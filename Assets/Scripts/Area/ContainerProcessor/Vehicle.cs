3445de99d65165ba263a693b28ea21276dd9cc22
Felix Yang <felixyang117+GitHub@hotmail.com>
2018-01-05 13:46:22 +0100

    asd

--- Assets/Scripts/Area/ContainerProcessor/Vehicle.cs
+++ Assets/Scripts/Area/ContainerProcessor/Vehicle.cs
@@ -3,18 +3,53 @@ using UnityEngine;
 
 public class Vehicle : MoveableObject
 {
+    // Upgrade related stuff
+    // They are static because I don't think we want user to upgrade individual AGV. Rather, upgrade all AGVs at once.
+    private static int countAGV_ = 0;
+    private static int level_ = 0;
+    private const int maxLevel_ = 4;
+    private static int[] costOfUpgrade_ = new int[maxLevel_ + 1] {0, 10, 20, 40, 80 };
+    private static float[] bonusOfUpgrade_ = new float[maxLevel_ + 1] {1.0f, 1.1f, 1.2f, 1.4f, 1.8f};
+    public static bool IsFullyUpgraded() { return ( level_ >= maxLevel_ ? true : false ); }
+    public static int PeekNextUpgradeCost() { return ( IsFullyUpgraded() ? -1 : costOfUpgrade_[level_ + 1] ); } // Return -1 if already maxed out
+    public static float PeekNextUpgradeBonus() { return (IsFullyUpgraded() ? -1 : bonusOfUpgrade_[level_ + 1]); } // Return -1 if already maxed out
+    public static bool CanAffordNextUpgrade() { return ( GetMoney() >= PeekNextUpgradeCost() * countAGV_ + 0.01 ? true : false ); } // Note: Do we really need money to be double? I think we should make it int.
+    public static bool CanUpgrade() { return ((IsFullyUpgraded() || !CanAffordNextUpgrade()) ? false : true); }
+    public static bool Upgrade()
+    {
+        if (CanUpgrade())
+        {
+            Debug.Log("\nMoney: " + GetMoney() + "\nVehicleCount: " + countAGV_ + "\nCurrentSpeed: " + initSpeed_ + "\nUnitUpgradeCost:" + PeekNextUpgradeCost() + "\nTotalUpgradeCost" + PeekNextUpgradeCost() * countAGV_);
+            SubtractMoney(PeekNextUpgradeCost() * countAGV_);
+            initSpeed_ *= PeekNextUpgradeBonus();
+            ++level_;
+            Debug.Log("\nMoney: " + GetMoney() + "\nVehicleCount: " + countAGV_ + "\nCurrentSpeed: " + initSpeed_);
+            return true;
+        }
+        else return false;
+    }
+    private void Start()
+    {
+        ++countAGV_;
+        //Debug.Log("AGV Count: " + countAGV_);
+    }
+    private static float initSpeed_ = 5.0f;
+    // TODO: Impl these
+    private static double GetMoney() { return 999.0; }
+    private static void SubtractMoney(int i_amount) { int a = i_amount; }
+    // End of upgrade related stuff
+
     private Game game;
     public Road road;
     public int capacity;
     public List<MonoContainer> containers = new List<MonoContainer>();
-    public float speed;
     private Area targetArea;
     public Queue<Area> request = new Queue<Area>();
 
     private void Awake() {
         request = new Queue<Area>();
         game = (Game) FindObjectOfType(typeof(Game));
-        MOInit(transform.position, speed, false);
+        MOInit(transform.position, 5.0f, false);
     }
 
     public bool AddContainer(MonoContainer monoContainer)
