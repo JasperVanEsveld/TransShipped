0695ce14a77c5429ed92e5b4b225d81118d8fa15
Jasper van Esveld <jaspervanesveld@gmail.com>
2018-01-08 12:13:24 +0100

    Add propper getter and setter money
    You can now use either SetMoney(new_value) or money = new_value

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -13,7 +13,18 @@ public class Game : MonoBehaviour
     public List<Stage> stagesList;
     public Queue<Stage> stages;
     public int movements;
-    public double money;
+    private double moneyValue = 100;
+    public double money {
+        get{
+            return this.moneyValue;
+        }
+        set{
+            this.moneyValue = value;
+            if(moneyChangeEvent != null){
+                moneyChangeEvent(money);
+            }
+        }
+    }
     public List<DeliveryVehicle> vehicles = new List<DeliveryVehicle>();
     public List<OptionalArea> optionalAreas = new List<OptionalArea>();
     private readonly List<Area> areas = new List<Area>();
@@ -88,9 +99,6 @@ public class Game : MonoBehaviour
 
     public void SetMoney(double money) {
         this.money = money;
-        if(moneyChangeEvent != null){
-            moneyChangeEvent(money);
-        }
     }
 
     public void SetStage(Stage newStage) {
