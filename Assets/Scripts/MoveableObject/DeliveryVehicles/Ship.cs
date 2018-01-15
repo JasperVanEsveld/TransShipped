abd8aa4231591f2c5fb47eff00ea653542feeae2
Shang Xiang <CoolTomatos@iCloud.com>
2018-01-15 15:23:52 +0100

    Stacking containers on a ship
    Signed-off-by: Shang Xiang <CoolTomatos@iCloud.com>

--- Assets/Scripts/MoveableObject/DeliveryVehicles/Ship.cs
+++ Assets/Scripts/MoveableObject/DeliveryVehicles/Ship.cs
@@ -1,25 +1,23 @@
 using System.Collections.Generic;
+using UnityEngine;
 
-public class Ship : DeliveryVehicle
-{
+public class Ship : DeliveryVehicle {
     public ShipArea area;
 
     public override void LeaveTerminal() {
         MOShipLeaveTerminal();
     }
 
-    public override void OnSelected()
-    {
+    public override void OnSelected() {
         List<ShipArea> areas = Game.OnlyHighlight<ShipArea>();
-        foreach(ShipArea currentArea in areas){
-            if( currentArea.occupied) {
+        foreach (ShipArea currentArea in areas) {
+            if (currentArea.occupied) {
                 currentArea.Highlight(false);
             }
         }
     }
 
-    private void Start()
-    {
+    private void Start() {
         MOInit(shipSpawnPos, 20, true);
 
         List<ShipArea> areaList = Game.GetAreasOfType<ShipArea>();
@@ -28,14 +26,24 @@ public class Ship : DeliveryVehicle
         area = areaList[0];
     }
 
-    protected override void DestroyIfDone(){
-        if(isAtDestination && MOIsAtTheThisPos(shipSpawnPos)){
-            Destroy(this.gameObject);
+    protected override void DestroyIfDone() {
+        if (isAtDestination && MOIsAtTheThisPos(shipSpawnPos)) {
+            Destroy(gameObject);
         }
     }
 
-    protected override void Enter()
-    {
+    protected override void Enter() {
         area.OnVehicleEnter(this);
+    }
+
+    private void Update() {
+        for (var i = 0; i < carrying.Count; i++) {
+            var n = i % 9;
+            carrying[i].transform.position = new Vector3(
+                (float) (transform.position.x - 1.4 + n / 3 * 2),
+                (float) (transform.position.y + i / 9 + 2.6),
+                transform.position.z - 1 + i % 3
+            );
+        }
     }
 }
